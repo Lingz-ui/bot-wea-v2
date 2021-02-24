0811cc16143e94e699936dd6cdc7644dd764214d
L-Mo <78338200+L-M0@users.noreply.github.com>
2021-02-24 14:43:29 +0700

    Add files via upload

---
+++ src/gcmenu.js
@@ -0,0 +1,14 @@
+const gcmenu = (prefix) => { 
+	return `
+	
+╔<[ *group MENU* ]>
+║
+╠➤ ${prefix}Kick @
+╠➤ ${prefix}promote @
+╠➤ ${prefix}demote @
+╠➤ ${prefix}tagall
+╠➤ ${prefix}add 62×××××
+╚➤ ${prefix}hidetag
+`
+}
+exports.gcmenu = gcmenu
